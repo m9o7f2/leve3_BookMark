654c6f5b54b97357e1911becdeec8ab930c2d889
Your Name <m9o7f2@gmail.com>
2020-07-21 21:36:34 +0300

    upload

---
+++ js/index.js
@@ -0,0 +1,40 @@
+// hide sho wtaible
+
+function hide_show (){
+  var li = document.querySelectorAll(".togle ul li"),
+      table = document.querySelectorAll(".table");
+      li.forEach(el => {
+        el.addEventListener('click' , function(e){
+          el.parentNode.querySelectorAll(".active").forEach(liAct => {
+            liAct.classList.remove("active");
+          })
+          e.target.classList.add("active")
+          table.forEach(tab => {
+            if (e.target.getAttribute("data-show") == tab.getAttribute("data-show")){
+              tab.parentNode.querySelectorAll('.active').forEach(act => {
+                act.classList.remove("active")
+                //console.log(act)
+              })
+              tab.classList.add("active")
+            }  
+          })        
+        })
+      })
+}
+
+hide_show()
+
+function prShow(){
+  var headel = document.querySelectorAll(".drop .tit");
+    //console.log(headel)
+
+    headel.forEach(el =>{
+      el.addEventListener('click',function(e){
+        e.target.parentNode.parentNode.parentNode.querySelectorAll(".h-auto").forEach(test =>{
+          test.classList.remove('h-auto')
+        })
+        e.target.parentNode.nextSibling.classList.add("h-auto")
+      })
+    })
+}
+prShow()
